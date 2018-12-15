9bdebf8c82e93debb24f56922a6d3ee6b96c4752
obudova <olgabudova55@gmail.com>
2018-12-15 19:42:37 +0200

    feat(user-dashboard): add container

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserDashboardModule } from './user-dashboard/user-dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserDashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/user-dashboard/user-dashboard.module.ts
+++ frontend/src/app/user-dashboard/user-dashboard.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserDashboardComponent } from './containers/user-dasboard/user-dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    UserDashboardComponent
+  ],
+  exports: [
+    UserDashboardComponent
+  ]
 })
 export class UserDashboardModule { }
