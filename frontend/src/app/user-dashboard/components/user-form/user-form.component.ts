1938c133551544582552971b9c5c3ad5c06b3e9c
obudova <olgabudova55@gmail.com>
2018-12-19 23:31:35 +0200

    feat(user-form): add rfc standard email pattern

--- frontend/src/app/user-dashboard/components/user-form/user-form.component.ts
+++ frontend/src/app/user-dashboard/components/user-form/user-form.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { User } from '../../../../core/models/user.interface';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { User } from '../../../../core/models/user.interface';
+const emailPattern =
+  '^(([^<>()\\[\\]\\\\.,;:\\s@"]+' +
+  '(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|' +
+  '(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$';
 
 @Component({
   selector: 'app-user-form',
@@ -24,7 +29,7 @@ export class UserFormComponent implements OnInit {
       surname: this.fb.control(this.user && this.user.surname, Validators.required),
       birth_date: this.fb.control(this.user && new Date(this.user.birth_date)),
       phone_number: this.fb.control(this.user && this.user.phone_number),
-      email: this.fb.control(this.user && this.user.email, Validators.email),
+      email: this.fb.control(this.user && this.user.email, Validators.pattern(emailPattern)),
     };
     
     Object.keys(userForm).forEach((control) => {
